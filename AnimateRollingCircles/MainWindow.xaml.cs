1d45563bb33e4da6a6ad67b27426ef27aa92f4f3
OndraMarek <czondrejmarek@gmail.com>
2024-12-02 10:49:44 +0100

    wip

--- AnimateRollingCircles/MainWindow.xaml.cs
+++ AnimateRollingCircles/MainWindow.xaml.cs
@@ -9,6 +9,12 @@ namespace AnimateRollingCircles
         {
             InitializeComponent();
         }
+        public void AnimateRollingCircles(int x, int y, int radius1, int radius2)
+        {
+            ConvertCordinates(ref x, ref y);
+
+        }
+
         private void ConvertCordinates(ref int x, ref int y)
         {
             y = -y;
@@ -27,7 +33,12 @@ namespace AnimateRollingCircles
         }
         private void animateRollingCirclesButton_Click(object sender, RoutedEventArgs e)
         {
-            return;
+            ClearCanvas();
+            int x = xTextBox.Text != "" ? int.Parse(xTextBox.Text) : 0;
+            int y = yTextBox.Text != "" ? int.Parse(yTextBox.Text) : 0;
+            int radius1 = radius1TextBox.Text != "" ? int.Parse(radius1TextBox.Text) : 0;
+            int radius2 = radius2TextBox.Text != "" ? int.Parse(radius2TextBox.Text) : 0;
+            AnimateRollingCircles(x, y, radius1, radius2);
         }
     }
 }
