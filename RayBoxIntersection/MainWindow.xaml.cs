358eb4d5db27efb497e099932ed382435b3815cc
OndraMarek <czondrejmarek@gmail.com>
2025-03-14 16:40:04 +0100

    3d render using HelixToolkit

--- RayBoxIntersection/MainWindow.xaml.cs
+++ RayBoxIntersection/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
 
 namespace RayBoxIntersection
 {
@@ -8,89 +9,78 @@ namespace RayBoxIntersection
     {
         public MainWindow()
         {
+            int[] b = [0, 0, 0];
+            int[] v = [50, 100, 150];
+
+
             InitializeComponent();
-            DrawCuboid([0, 0, 0], [200, 100, 100]);
+
+            DrawCube(b[0], b[1], b[2], v[0], v[1], v[2]);
+            SetupCamera(b[0], b[1], b[2], v[0], v[1], v[2]);
         }
 
-        private void ConvertCoordinates(ref double x, ref double y)
+        private void DrawCube(int posX, int posY, int posZ, int width, int length, int height)
         {
-            y = -y;
-            x += drawCanvas.Width / 2;
-            y += drawCanvas.Height / 2;
-        }
+            var lines = new LinesVisual3D { Color = Colors.Blue, Thickness = 2 };
 
-        private void ClearCanvas()
-        {
-            drawCanvas.Children.Clear();
-        }
+            Point3D[] vertices =
+            [
+                new Point3D(posX, posY, posZ),
+                new Point3D(posX + width, posY, posZ),
+                new Point3D(posX + width, posY + length, posZ),
+                new Point3D(posX, posY + length, posZ),
+                new Point3D(posX, posY, posZ + height),
+                new Point3D(posX + width, posY, posZ + height),
+                new Point3D(posX + width, posY + length, posZ + height),
+                new Point3D(posX, posY + length, posZ + height)
+            ];
 
-        private void DrawCuboid(double[] b, double[] v)
-        {
-            double x = b[0];
-            double y = b[1];
-            double z = b[2];
-            double width = v[0];
-            double height = v[1];
-            double depth = v[2];
-
-            ConvertCoordinates(ref x, ref y);
-
-            x = x + (z / 2);
-            y = y - (z / 2);
-
-            double offsetX = depth / 2;
-            double offsetY = depth / 2;
-
-            double x1 = x;
-            double y1 = y + height;
-            double x2 = x + width;
-            double y2 = y + height;
-            double x3 = x + width;
-            double y3 = y;
-            double x4 = x;
-            double y4 = y;
-
-            double x5 = x + offsetX;
-            double y5 = y + height - offsetY;
-            double x6 = x + width + offsetX;
-            double y6 = y + height - offsetY;
-            double x7 = x + width + offsetX;
-            double y7 = y - offsetY;
-            double x8 = x + offsetX;
-            double y8 = y - offsetY;
-
-            DrawLine(x1, y1, x2, y2);
-            DrawLine(x2, y2, x3, y3);
-            DrawLine(x3, y3, x4, y4);
-            DrawLine(x4, y4, x1, y1);
-
-            DrawLine(x5, y5, x6, y6, true);
-            DrawLine(x6, y6, x7, y7);
-            DrawLine(x7, y7, x8, y8);
-            DrawLine(x8, y8, x5, y5, true);
-
-            DrawLine(x1, y1, x5, y5, true);
-            DrawLine(x2, y2, x6, y6);
-            DrawLine(x3, y3, x7, y7);
-            DrawLine(x4, y4, x8, y8);
-        }
-
-        private void DrawLine(double x1, double y1, double x2, double y2, bool isDashed = false)
-        {
-            Line line = new Line
+            int[,] edges = new int[,]
             {
-                X1 = x1,
-                Y1 = y1,
-                X2 = x2,
-                Y2 = y2,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1
+                {0,1}, {1,2}, {2,3}, {3,0},
+                {4,5}, {5,6}, {6,7}, {7,4},
+                {0,4}, {1,5}, {2,6}, {3,7}
             };
-            if (isDashed)
+
+            for (int i = 0; i < edges.GetLength(0); i++)
             {
-                line.StrokeDashArray = new DoubleCollection() { 4, 4 };
+                lines.Points.Add(vertices[edges[i, 0]]);
+                lines.Points.Add(vertices[edges[i, 1]]);
             }
-            drawCanvas.Children.Add(line);
+
+            viewport.Children.Add(lines);
+        }
+
+        private void SetupCamera(int posX, int posY, int posZ, int width, int length, int height)
+        {
+            Point3D center = new Point3D(
+                posX + width / 2.0,
+                posY + length / 2.0,
+                posZ + height / 2.0
+            );
+
+            double maxDimension = Math.Max(Math.Max(width, length), height);
+            double cameraDistance = maxDimension * 2;
+
+            Point3D cameraPosition = new Point3D(
+                center.X + cameraDistance,
+                center.Y + cameraDistance,
+                center.Z + cameraDistance
+            );
+
+            PerspectiveCamera camera = new PerspectiveCamera
+            {
+                Position = cameraPosition,
+                LookDirection = new Vector3D(
+                    center.X - cameraPosition.X,
+                    center.Y - cameraPosition.Y,
+                    center.Z - cameraPosition.Z
+                ),
+                UpDirection = new Vector3D(0, 1, 0),
+                FieldOfView = 60
+            };
+
+            viewport.Camera = camera;
         }
     }
 }
