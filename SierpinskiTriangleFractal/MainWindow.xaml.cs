bedd479123da0311fa42b2445985deb8f755b647
OndraMarek <czondrejmarek@gmail.com>
2025-05-10 22:25:40 +0200

    SierpinskiTriangleFractal inicialization

---
+++ SierpinskiTriangleFractal/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+
+namespace SierpinskiTriangleFractal
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void DrawSierpinskiTriangleRecursive(int iterations)
+        {
+            
+        }
+
+        private void DrawSierpinskiTriangle_Click(object sender, RoutedEventArgs e)
+        {
+            drawSierpinskiTriangleCanvas.Children.Clear();
+            int iterations = iterationsTextBox.Text != "" ? int.Parse(iterationsTextBox.Text) : 0;
+            DrawSierpinskiTriangleRecursive(iterations);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]+$");
+        }
+    }
+}
