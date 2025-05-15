bb5d3bad9174af5d59869b6cbfa2d6032a3326f0
OndraMarek <czondrejmarek@gmail.com>
2025-05-15 21:52:10 +0200

    inicialize MandelbrotSetFractal

---
+++ MandelbrotSetFractal/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MandelbrotSetFractal
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
