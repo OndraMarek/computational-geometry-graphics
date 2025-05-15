7d9f4b22e6fa959c540f0f6e402fd20636b904c3
OndraMarek <czondrejmarek@gmail.com>
2025-05-15 21:54:35 +0200

    wip

--- MandelbrotSetFractal/MainWindow.xaml.cs
+++ MandelbrotSetFractal/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MandelbrotSetFractal
 {
@@ -7,6 +9,21 @@ namespace MandelbrotSetFractal
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DrawMandelbrotSet_Click_Click(object sender, RoutedEventArgs e)
+        {
+            int iterations = iterationsTextBox.Text != "" ? int.Parse(iterationsTextBox.Text) : 0;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]+$");
         }
     }
 }
